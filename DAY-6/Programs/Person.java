5fe4eff0705e3dfad9dbf87162e0aff38704ab8e
nikitha1611 <164365096+nikitha1611@users.noreply.github.com>
2024-03-30 14:48:50 +0530

    Create Person.java

---
+++ DAY-6/Programs/Person.java
@@ -0,0 +1,12 @@
+package com.targetindia.programs;
+import lombok.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Person {
+    private String name;
+    private String address;
+
+}
